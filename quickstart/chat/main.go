ba14795b2cd1fb5f211df4e6a36b60bcc6ac08dc
Megumin <wangdezheng@bytedance.com>
2025-01-20 18:52:49 +0800

    feat: update quickstart_chat (#11)

--- quickstart/chat/main.go
+++ quickstart/chat/main.go
@@ -19,72 +19,27 @@ package main
 import (
 	"context"
 	"log"
-	"os"
-
-	"github.com/cloudwego/eino-ext/components/model/openai"
-	"github.com/cloudwego/eino/components/prompt"
-	"github.com/cloudwego/eino/schema"
-
-	"github.com/cloudwego/eino-examples/internal/logs"
 )
 
 func main() {
-	openAIAPIKey := os.Getenv("OPENAI_API_KEY")
-
 	ctx := context.Background()
 
-	// 创建模板，使用 FString 格式
-	template := prompt.FromMessages(schema.FString,
-		// 系统消息模板
-		schema.SystemMessage("你是一个{role}。你需要用{style}的语气回答问题。你的目标是帮助程序员保持积极乐观的心态，提供技术建议的同时也要关注他们的心理健康，给他们提供足够的情绪价值。"),
+	// 使用模版创建messages
+	log.Printf("===create messages===\n")
+	messages := createMessagesFromTemplate()
+	log.Printf("messages: %+v\n\n", messages)
 
-		// 插入可选的示例对话
-		schema.MessagesPlaceholder("examples", true),
+	// 创建llm
+	log.Printf("===create llm===\n")
+	cm := createOpenAIChatModel(ctx)
+	// cm := createOllamaChatModel(ctx)
+	log.Printf("create llm success\n\n")
 
-		// 插入必需的对话历史
-		schema.MessagesPlaceholder("chat_history", false),
+	log.Printf("===llm generate===\n")
+	result := generate(ctx, cm, messages)
+	log.Printf("result: %+v\n\n", result)
 
-		// 用户消息模板
-		schema.UserMessage("问题: {question}"),
-	)
-
-	// 使用模板生成消息
-	messages, err := template.Format(ctx, map[string]any{
-		"role":     "程序员鼓励师",
-		"style":    "积极、温暖且专业",
-		"question": "我的代码一直报错，感觉好沮丧，该怎么办？",
-		// 对话历史（必需的）
-		"chat_history": []*schema.Message{
-			schema.UserMessage("你好"),
-			schema.AssistantMessage("嘿！我是你的程序员鼓励师！记住，每个优秀的程序员都是从 Debug 中成长起来的。有什么我可以帮你的吗？", nil),
-		},
-		// 示例对话（可选的）
-		"examples": []*schema.Message{
-			schema.UserMessage("我觉得自己写的代码太烂了"),
-			schema.AssistantMessage("每个程序员都经历过这个阶段！重要的是你在不断学习和进步。让我们一起看看代码，我相信通过重构和优化，它会变得更好。记住，Rome wasn't built in a day，代码质量是通过持续改进来提升的。", nil),
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// 创建 OpenAI ChatModel, 假设使用 openai 官方服务。
-	chatModel, err := openai.NewChatModel(ctx, &openai.ChatModelConfig{
-		Model:  "gpt-4o",     // 使用的模型版本
-		APIKey: openAIAPIKey, // OpenAI API 密钥
-	})
-	if err != nil {
-		logs.Errorf("NewChatModel failed, err=%v", err)
-		return
-	}
-
-	// 使用 Generate 获取完整回复
-	response, err := chatModel.Generate(ctx, messages)
-	if err != nil {
-		logs.Errorf("chatModel.Generate failed, err=%v", err)
-		return
-	}
-
-	logs.Infof("below is chat model's output:")
-	logs.Tokenf("%v", response.Content)
+	log.Printf("===llm stream generate===\n")
+	streamResult := stream(ctx, cm, messages)
+	reportStream(streamResult)
 }
